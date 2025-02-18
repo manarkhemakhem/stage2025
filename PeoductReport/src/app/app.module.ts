992f17c549cefe8eee1aa79fdddceb165cdea177
manar khemakhem <manarkhemakhem0@gmail.com>
2025-02-18 10:42:28 +0100

    Sauvegarde avant mise à jour Angular Material

--- PeoductReport/src/app/app.module.ts
+++ PeoductReport/src/app/app.module.ts
@@ -5,29 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProduitsComponent } from './produits/produits.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CreateproductComponent } from './createproduct/createproduct.component';
 import { EditproductComponent } from './editproduct/editproduct.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ProduitsComponent,
-    CreateproductComponent,
-    EditproductComponent
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    CommonModule,
-    RouterModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        ProduitsComponent,
+        CreateproductComponent,
+        EditproductComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        CommonModule,
+        RouterModule,
+        ReactiveFormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
